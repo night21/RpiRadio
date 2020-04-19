fc2e68a1c6c2873f107380898a5d248ac623a8ae
night21 <night21@gmail.com>
2020-04-19 13:59:50 +0200

    Initial console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RpiRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
